2b61328b2dd1a34fa13f279afc7fb8b23ac1bc1d
Ola Næss Kaldestad <mail@okal.no>
2023-12-14 00:24:32 +0100

    fix undefined in cache issue

--- lib/memoize.ts
+++ lib/memoize.ts
@@ -1,17 +1,9 @@
-type Cache<T> = { [key: string]: T | undefined };
-
-// deno-lint-ignore no-explicit-any
-export function memoize<F extends (...args: any[]) => any>(func: F, cache: Cache<ReturnType<F>> = {}) {
-    return function(...args: Parameters<F>): ReturnType<F> {
-        const key = JSON.stringify(args);
-        const cached = cache[key];
-
-        if (cached !== undefined) {
-            return cached;
-        }
-
-        return cache[key] = func(...args) as ReturnType<F>;
-    };
+export const memoize = <P extends unknown[], R>(
+    f: (...p: P) => R,
+    c: Record<string, R> = {},
+) => (...p: P) => {
+    const k = JSON.stringify(p);
+    return k in c ? c[k] : c[k] = f(...p);
 };
 
 export default memoize;
